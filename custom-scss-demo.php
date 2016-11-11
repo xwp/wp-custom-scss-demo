fb1693d6c79717456e9c7f6b4b5c4b4e8cc24f44
Weston Ruter <weston@xwp.co>
2016-11-11 12:27:50 -0800

    Use filters instead of subclassing custom_css setting

--- custom-scss-demo.php
+++ custom-scss-demo.php
@@ -29,6 +29,92 @@
 namespace CustomScssDemo;
 
 /**
+ * Add filter for wp_get_custom_css to transpile SCSS into CSS.
+ */
+function customize_preview_init() {
+	add_filter( 'wp_get_custom_css', __NAMESPACE__ . '\filter_previewed_custom_scss', 10, 2 );
+}
+add_action( 'customize_preview_init', __NAMESPACE__ . '\customize_preview_init' );
+
+/**
+ * Filter custom_css to transpile.
+ *
+ * @param string $css CSS
+ * @return string CSS.
+ */
+function filter_previewed_custom_scss( $css ) {
+	global $wp_customize;
+
+	$preprocessor_setting = $wp_customize->get_setting( 'custom_css_preprocessor' );
+	if ( $preprocessor_setting && 'scss' === $preprocessor_setting->value() ) {
+		$css = transpile_scss( $css );
+	}
+	return $css;
+}
+
+/**
+ * Transpile SCSS.
+ *
+ * @param string $scss SCSS.
+ * @return string CSS.
+ */
+function transpile_scss( $scss ) {
+	if ( ! class_exists( 'scssc' ) ) {
+		require_once __DIR__ . '/lib/scss.inc.php';
+	}
+	$scss_compiler = new \scssc();
+	return $scss_compiler->compile( $scss );
+}
+
+/**
+ * Filter customize value for custom_css setting.
+ *
+ * If a SCSS preprocessor is being used, then the CSS is pulled from `post_content_filtered`
+ * which contains the pre-processed source. Note that if the value is customized, then this
+ * filter will not apply as the post_value will be used directly.
+ *
+ * @param string $css CSS value.
+ * @param \WP_Customize_Custom_CSS_Setting $setting
+ * @return string SCSS value.
+ */
+function filter_customize_value( $css, \WP_Customize_Custom_CSS_Setting $setting ) {
+	$preprocessor_setting = $setting->manager->get_setting( 'custom_css_preprocessor' );
+
+	if ( ! $preprocessor_setting || 'scss' !== $preprocessor_setting->value() ) {
+		return $css;
+	}
+
+	$post = wp_get_custom_css_post( $setting->stylesheet );
+	if ( $post && ! empty( $post->post_content_filtered ) ) {
+		$css = $post->post_content_filtered;
+	}
+
+	return $css;
+}
+
+/**
+ * Filter post content args for `custom_css` being updated.
+ *
+ * Stores SCSS in `post_content_filtered` and transpiled CSS in `post_content`.
+ * The reverse operation is done in `filter_customize_value()` when reading the
+ * value out of the `custom_css` post.
+ *
+ * @param array                            $post_content_args The post_content_args.
+ * @param string                           $css               The (S)CSS being saved.
+ * @param \WP_Customize_Custom_CSS_Setting $setting           Custom CSS Setting.
+ * @return array Post content args.
+ */
+function filter_custom_css_post_content_args( $post_content_args, $css, $setting ) {
+	unset( $css );
+	$preprocessor_setting = $setting->manager->get_setting( 'custom_css_preprocessor' );
+	if ( $preprocessor_setting && 'scss' === $preprocessor_setting->post_value( $preprocessor_setting->value() ) ) {
+		$post_content_args['post_content_filtered'] = $post_content_args['post_content']; // Stash original SCSS in post_content_filtered.
+		$post_content_args['post_content'] = transpile_scss( $post_content_args['post_content'] );
+	}
+	return $post_content_args;
+}
+
+/**
  * Enqueue preview scripts.
  */
 function enqueue_preview_scripts() {
@@ -37,12 +123,10 @@ function enqueue_preview_scripts() {
 	}
 	$handle = 'custom-scss-demo-preview';
 	$src = plugin_dir_url( __FILE__ ) . 'custom-scss-demo-preview.js';
-	$deps = array( 'customize-preview' );
+	$deps = array( 'customize-preview', 'customize-selective-refresh' );
 	wp_enqueue_script( $handle, $src, $deps );
 }
-add_action( 'customize_preview_init', function() {
-	add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_preview_scripts' );
-} );
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_preview_scripts' );
 
 /**
  * Register controls.
@@ -83,13 +167,11 @@ function register_controls( \WP_Customize_Manager $wp_customize ) {
 		'priority' => 10,
 	) );
 
-	require_once __DIR__ . '/class-customize-setting.php';
-	$custom_scss_setting = new Customize_Setting( $wp_customize, $custom_css_setting->id, wp_array_slice_assoc(
-		get_object_vars( $custom_css_setting ),
-		array( 'default', 'capability' )
-	) );
-	$wp_customize->remove_setting( $custom_css_setting->id );
-	$wp_customize->add_setting( $custom_scss_setting );
+	// Ensure that the post_content_filtered is used as the value when SCSS is chosen as the pre-processor.
+	add_filter( 'customize_value_custom_css', __NAMESPACE__ . '\filter_customize_value', 10, 2 );
+
+	// Ensure that the SCSS gets stored in post_content_filtered, and transpiled CSS in post_content.
+	add_filter( 'customize_update_custom_css_post_content_args', __NAMESPACE__ . '\filter_custom_css_post_content_args', 10, 3 );
 }
 add_action( 'customize_register', __NAMESPACE__ . '\register_controls', 20 );
 
